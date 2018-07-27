d317890afb0f2e9b0f4c39fee23af70e67d6360c
Seungyeon Kim <loycord.ceo@gmail.com>
2018-07-27 19:01:48 +0900

    test debugger

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
-export default () => <div>Welcome to next.js</div>
+import React from 'react';
+import cowsay from 'cowsay-browser';
+
+console.log('baby');
+
+export default class extends React.Component {
+  render() {
+    console.log('hi there');
+    const a = 3;
+    const b = 5;
+    const c = a + b;
+
+    return <pre>{cowsay.say({ text: 'hi there!' })}</pre>;
+  }
+}
